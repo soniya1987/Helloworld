5e8cf60b2307209f91d473c32c3358b6627d46e6
Soniya Vakkat Kuttan <vksoniya68@gmail.com>
2022-08-03 17:00:58 -0500

    Helloworld implemented

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "!");
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
