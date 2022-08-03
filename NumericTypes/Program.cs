2f94d4deaaf7b329dc3a9e64f5a4099908e02216
Soniya Vakkat Kuttan <vksoniya68@gmail.com>
2022-08-03 17:31:30 -0500

    Strings implemented

---
+++ NumericTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NumericTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the length of the Rectangle :");
+            int length = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is the width of the Rectangle :");
+            int width = int.Parse(Console.ReadLine());
+
+            //int area = length * width;
+
+            Console.WriteLine("Area of Rectangle is " + AreaOfRectangle(length, width));
+        }
+
+        static int AreaOfRectangle(int length, int width)
+        {
+            return length * width;
+        }
+    }
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string alice = @"Alice was beginning to get very tired of sitting by her sister on the
+   bank, and of having nothing to do: once or twice she had peeped into the
+   book her sister was reading, but it had no pictures or conversations in
+   it, 'and what is the use of a book,' thought Alice 'without pictures or
+   conversation?'";
+
+            Console.WriteLine(alice);
+            Console.WriteLine("What sentence would you like to look for in the sentence above?");
+            string searchTerm = Console.ReadLine();
+            string compSearchTerm = searchTerm.ToLower();
+            string compAlice = alice.ToLower();
+
+            if (compAlice.IndexOf(compSearchTerm, 0) != -1)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
