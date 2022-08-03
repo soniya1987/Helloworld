6294ca753d0e085fc108563e71f9735195bb6875
Soniya Vakkat Kuttan <vksoniya68@gmail.com>
2022-08-03 17:23:32 -0500

    More on NumericTypes implemented

---
+++ MoreOnNumericTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MoreOnNumericTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many miles did you drive on your trip?");
+            string mi = Console.ReadLine();
+            int miles = Int32.Parse(mi);
+
+            Console.WriteLine("How many gallons of gas did you use?");
+            string gal = Console.ReadLine();
+            int gallons = Int32.Parse(gal);
+
+            int mpg = miles / gallons;
+            Console.WriteLine("The MPG for the trip was: " + mpg);
+        }
+    }
+}
